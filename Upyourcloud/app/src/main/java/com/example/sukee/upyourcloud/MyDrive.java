e57ee3034b2621c52710daa13e5398001b7d6353
AngryPotato <sukeerth14@gmail.com>
2016-09-07 15:52:33 +0530

    Actual project

---
+++ Upyourcloud/app/src/main/java/com/example/sukee/upyourcloud/MyDrive.java
@@ -0,0 +1,13 @@
+package com.example.sukee.upyourcloud;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyDrive extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_drive);
+    }
+}
