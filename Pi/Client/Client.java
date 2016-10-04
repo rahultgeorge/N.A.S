0073c1ce7e49fc0bcc600ac381cabb202920ecb4
rahultgeorge <rahultitusgeorge2011@gmail.com>
2016-10-04 21:22:39 +0530

    Server and Client File transferwith backup and retrieve options

---
+++ Pi/Client/Client.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Client
+{
+	private static final int port = 1234;
+	static DataInputStream in = null;
+	static DataOutputStream out = null;
+	private static final String ret="retrieve";
+	private static final String exit="exit";
+
+	public static void main(String []args)throws IOException
+	{
+		byte[] buffer ;
+               float bytesRead=0;
+		float kb=0;
+		float count=0;
+		long file_size=0;
+ 		Scanner sc=new Scanner(System.in);
+		String f_name;
+		Socket s = new Socket("192.168.1.35", port);
+               while(true)
+              {
+               System.out.println("Welcome Laptop");
+		System.out.println("Enter 1 to backup to the pi");
+		System.out.println("      2 to retrieve from the pi");
+		int c=sc.nextInt();
+		               
+		if(c==1)
+               {
+                //Send to the pi
+		System.out.println("Enter the file name to send");
+	        File file = new File(sc.next());							System.out.println("File name entered is: "+file.getName());
+		in = new DataInputStream(new FileInputStream(file));
+		out= new  DataOutputStream(s.getOutputStream());
+		out.writeUTF(file.getName());	
+		file_size=(file.length()/1024);
+		System.out.println("File size(KB): "+file_size);
+		out.writeLong(file_size);	
+		//out.flush();
+               }
+		else if(c==2)
+		{
+                 //Retrieve 
+			System.out.println("Enter the file name to retrieve from the PI");
+			f_name=sc.next();
+			out= new  DataOutputStream(s.getOutputStream());
+			out.writeUTF(ret);
+			out.writeUTF(f_name);
+			in=new DataInputStream(s.getInputStream());          
+			out=new DataOutputStream(new FileOutputStream(in.readUTF()));
+			file_size=in.readLong();
+		}
+               else
+                {
+		       System.out.println("Socket connection terminated");
+                      break;
+                }
+		buffer=new byte[1024*1024];
+		count=0;
+               System.out.println("Writing has begun");
+	                while(count<=file_size && (bytesRead=in.read(buffer))>0 )
+		       {											System.out.println("Bytes read: "+ bytesRead);
+			out.write(buffer,0,(int)bytesRead);
+			kb=bytesRead/1024;
+			System.out.println("KB read: "+kb);
+			count+=kb;
+			System.out.println("Total writen to socket os(KB): "+count);	
+		       }	      
+		out.flush();
+          }
+		out.close();
+               in.close();
+		s.close();
+	}
+}
+		
--- Pi/Pi.java
+++ Pi/Pi.java
@@ -5,62 +5,101 @@ import java.util.*;
 public class Pi
 {
         private static final int port = 1234;
-        static DataInputStream in = null;
-        static DataOutputStream out = null;
+	private static final String ret="retrieve";
+	private static final String exit="exit";
 
         public static void main(String []args)throws IOException
         {
                 ServerSocket ss=new ServerSocket(port);
                 ss.setSoTimeout(10000);
-                System.out.println("Waiting to connect");
-                Socket s=ss.accept();
-                System.out.println("Connected");
                 Scanner sc = new Scanner(System.in);
                 int inp;
-                File f;
+                File f=null;
                 byte[] buffer;
-                int bytesRead,count=0;
+                float count=0;
+		 float kb=0;
+		 float bytesRead = 0;
 		 long file_size=0;
-                for(;;)
-                {
-                        System.out.println("Enter 1 to send, 2 to receive, 3 to exit\n");
-                        inp = sc.nextInt();
-                        if(inp == 1)
-                        {
-                                System.out.println("Enter the name of the file to send : ");
-                                f = new File(sc.next());
-                                in = new DataInputStream(new FileInputStream(f));
-                                out = new DataOutputStream(s.getOutputStream());
-				  out.writeUTF(f.getName());
-				  out.writeLong(f.length());
-				  out.flush();
-				  file_size=f.length();
-                        }
-                        else if(inp == 2)
-                        {
-                                in = new DataInputStream(s.getInputStream());
-                             out = new DataOutputStream(new FileOutputStream(in.readUTF()));
-				
-				file_size=in.readLong();
-				
-                        }
-                        else
-		        {
-                                s.close();
-                        out.close();
-                        in.close();
-                                break;
-                        }
+		 String f_name=null;
+		 buffer=new byte[1024*1024];
+		 DataInputStream s_in = null;
+         	 DataOutputStream s_out = null;
+	 	 DataInputStream f_in = null;
+         	 DataOutputStream f_out = null;
+		 System.out.println("Waiting to connect");
+                Socket s=ss.accept();
+                System.out.println("Connected");
+		 s_in = new DataInputStream(s.getInputStream());
+		 s_out = new DataOutputStream(s.getOutputStream());
 
-		buffer=new byte[1024];
-			count=0;
-	                while( count!=file_size && (bytesRead=in.read(buffer))>0 )
-		       {
-			out.write(buffer,0,bytesRead);
-			count+=bytesRead;
+		for(;;)
+		{
+		 try
+	{
+		if((f_name=s_in.readUTF())!=null && !f_name.equals(ret))
+		{
+			//Back up
+		f_out = new DataOutputStream(new FileOutputStream(f_name));
+		file_size=s_in.readLong();
+		System.out.println("FILE TO BE BACKED UP SIZE(KB): "+file_size);
+		count=0;
+		while( Math.ceil(count)<file_size && (bytesRead=s_in.read(buffer))>0 )
+		       {											System.out.println("Bytes read: "+ bytesRead);
+			f_out.write(buffer,0,(int)bytesRead);
+			kb=bytesRead/1024;
+			System.out.println("KB read: "+kb);
+			count+=kb;
+			System.out.println("Total writen into file (KB): "+count);
+			System.out.println(Math.ceil(count));
 		       }
-			out.flush();
-						
-                }
+			System.out.println("Successfull");
+
+			f_out.flush();
+		}
+               else if(f_name.equals(ret))
+		{
+			//Retrieve
+		  System.out.println("RETRIEVE");
+		  f_name=s_in.readUTF();
+		  f=new File(f_name);
+		  f_in = new DataInputStream(new FileInputStream(f_name));
+		  s_out.writeUTF(f_name);
+		  file_size=f.length()/(1024);
+		  System.out.println("FILE TO BE SENT SIZE(KB): "+file_size);
+		  s_out.writeLong(file_size);
+		  count=0;
+		   while( Math.ceil(count)<file_size && (bytesRead=f_in.read(buffer))>0 )
+		       {											System.out.println("Bytes read: "+ bytesRead);
+			s_out.write(buffer,0,(int)bytesRead);
+			kb=bytesRead/1024;
+			System.out.println("KB read: "+kb);
+			count+=kb;
+			System.out.println("Total writen to socket os(KB): "+count);
+
+		       }
+			
+			System.out.println("Success");
+			s_out.flush();
+
+		}
+		else if(f_name.equals(exit))
+		{
+		 //exit condition
+		  System.out.println("Socket go bye bye");
+		  s_out.close();
+		  s_in.close();
+		  s.close();
+		}
+		//send updates condition should be added
+		}
+		catch(EOFException e)
+		{
+		 System.out.println(e);
+		 System.out.println("Socket connection terminated");
+		 break;
+		}
+
+
+		}
         }
 }
